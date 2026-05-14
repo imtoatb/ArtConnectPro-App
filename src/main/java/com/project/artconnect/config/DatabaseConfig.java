a4581a356471b19e859847c232072cbcb0ebc93c
imtoatb <idfc.gfy@gmail.com>
2026-05-14 15:26:49 +0200

    Sauvegarde

--- src/main/java/com/project/artconnect/config/DatabaseConfig.java
+++ src/main/java/com/project/artconnect/config/DatabaseConfig.java
@@ -7,5 +7,5 @@ package com.project.artconnect.config;
 public class DatabaseConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/artconnect";
     public static final String USER = "root";
-    public static final String PASSWORD = "Patachou0+"; // CHANGE ME
+    public static final String PASSWORD = "M2S3_database"; // CHANGE ME
 }
